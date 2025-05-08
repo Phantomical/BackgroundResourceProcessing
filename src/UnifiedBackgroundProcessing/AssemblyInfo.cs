cfd9d69c46626e76b23faa2f3f5360c69ee4fbf2
Phantomical <phantom@lynches.ca>
2025-05-08 02:01:40 -0700

    Working OrTools build

---
+++ src/UnifiedBackgroundProcessing/AssemblyInfo.cs
@@ -0,0 +1,2 @@
+// [assembly: KSPAssemblyDependency("UnifiedBackgroundProcessing\\Google.OrTools", 9, 12)]
+// [assembly: KSPAssemblyDependency("UnifiedBackgroundProcessing\\Google.Protobuf", 3, 29)]
