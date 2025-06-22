9f31a6ae2c436afdf16207832b2930f7e703b08f
Phantomical <phantom@lynches.ca>
2025-06-22 16:21:12 -0700

    Add support for Kopernicus
    Kopernicus solar panels are their own special thing. This adds an
    integration + MM patch to replace ModuleBackgroundSolarPanel with a
    Kopernicus-specific version that interacts with the KopernicusSolarPanel
    module.

---
+++ src/BackgroundResourceProcessing.Integration.Kopernicus/BackgroundKopernicusSolarPanel.cs
@@ -0,0 +1,34 @@
+using BackgroundResourceProcessing.Modules;
+using Kopernicus.Components;
+
+namespace BackgroundResourceProcessing.Integration.Kopernicus
+{
+    public class ModuleBackgroundKopernicusSolarPanel : BackgroundConverterBase
+    {
+        protected override ConverterBehaviour GetConverterBehaviour()
+        {
+            // TODO:
+            //  - Support alternating between 0 and flowRate when going into and
+            //    out of planet shadows.
+            //  - Compute a non-zero background rate when in shadow.
+            //  - Be smarter when landed on a planet.
+            var netEC = 0.0;
+            var panels = GetComponents<KopernicusSolarPanel>();
+
+            foreach (var panel in panels)
+                netEC += panel.currentOutput;
+
+            var ratio = new ResourceRatio()
+            {
+                Ratio = netEC,
+                ResourceName = "ElectricCharge",
+                FlowMode = ResourceFlowMode.ALL_VESSEL_BALANCE,
+            };
+
+            if (netEC == 0.0)
+                return null;
+
+            return new ConstantProducer([ratio]);
+        }
+    }
+}
