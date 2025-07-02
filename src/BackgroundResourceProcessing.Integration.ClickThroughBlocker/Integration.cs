a2edd5809734ca66fc21b430bd8c487e8e527c0d
Phantomical <phantom@lynches.ca>
2025-07-02 01:25:11 -0700

    Update the CTB integration to no longer depend on Harmony

--- src/BackgroundResourceProcessing.Integration.ClickThroughBlocker/Integration.cs
+++ src/BackgroundResourceProcessing.Integration.ClickThroughBlocker/Integration.cs
@@ -1,6 +1,5 @@
-using System;
+using BackgroundResourceProcessing.UI;
 using ClickThroughFix;
-using HarmonyLib;
 using UnityEngine;
 
 namespace BackgroundResourceProcessing.Integration.ClickThroughBlocker
@@ -8,49 +7,23 @@ namespace BackgroundResourceProcessing.Integration.ClickThroughBlocker
     [KSPAddon(KSPAddon.Startup.Instantly, true)]
     internal class Loader : MonoBehaviour
     {
-        static Type GetDebugUIType()
+        void Start()
         {
-            var assembly = typeof(BackgroundResourceProcessor).Assembly;
-            return assembly.GetType("BackgroundResourceProcessing.UI.DebugUI");
+            DebugUI.WindowProvider = new ClickThroughBlockerWindowProvider();
         }
 
-        void Awake()
+        internal class ClickThroughBlockerWindowProvider : DebugUI.IGUIWindowProvider
         {
-            var harmony = new Harmony(
-                "BackgroundResourceProcessing.Integration.ClickThroughBlocker"
-            );
-
-            var type = GetDebugUIType();
-            if (type == null)
+            public Rect GUILayoutWindow(
+                int id,
+                Rect screenRect,
+                GUI.WindowFunction func,
+                string text,
+                GUIStyle style
+            )
             {
-                Debug.LogWarning(
-                    $"Unable to find DebugUI type within BackgroundResourceProcessing assembly"
-                );
-                return;
+                return ClickThruBlocker.GUILayoutWindow(id, screenRect, func, text, style);
             }
-
-            var method = AccessTools.Method(type, "GUILayoutWindow");
-            if (method == null)
-            {
-                Debug.LogWarning($"DebugUI has no method named GUILayoutWindow");
-                return;
-            }
-
-            var prefix = AccessTools.Method(GetType(), nameof(GUILayoutWindowPrefix));
-            harmony.Patch(method, new HarmonyMethod(prefix));
-        }
-
-        static bool GUILayoutWindowPrefix(
-            ref Rect __result,
-            int id,
-            Rect screenRect,
-            GUI.WindowFunction func,
-            string text,
-            GUIStyle style
-        )
-        {
-            __result = ClickThruBlocker.GUILayoutWindow(id, screenRect, func, text, style);
-            return false;
         }
     }
 }
