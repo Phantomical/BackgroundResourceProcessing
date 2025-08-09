b327b7cf35d7d12cd873eb5ff57d073562b1695f
Sean Lynch <phantom@lynches.ca>
2025-08-08 21:53:26 -0400

    Add inline option to KeyValuePairExt.Deconstruct

--- src/BackgroundResourceProcessing/Collections/DictionaryExtensions.cs
+++ src/BackgroundResourceProcessing/Collections/DictionaryExtensions.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace BackgroundResourceProcessing.Collections;
 
 internal static class KeyValuePairExt
 {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void Deconstruct<K, V>(this KeyValuePair<K, V> pair, out K key, out V value)
     {
         key = pair.Key;
