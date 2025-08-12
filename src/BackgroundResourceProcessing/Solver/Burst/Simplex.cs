1e3b37bceacc9bd143f9f78b3717f8e6000c95f4
Sean Lynch <phantom@lynches.ca>
2025-08-12 11:14:34 -0400

    Add avx2 accelerated version of presolve

--- src/BackgroundResourceProcessing/Solver/Burst/Simplex.cs
+++ src/BackgroundResourceProcessing/Solver/Burst/Simplex.cs
@@ -38,22 +38,13 @@ internal static partial class Simplex
 
             [BurstDiscard]
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            static bool Managed(ref bool trace) =>
+            static void Managed(ref bool trace) =>
                 trace = DebugSettings.Instance?.SolverTrace ?? false;
         }
     }
 
-#if BURST_TEST_OVERRIDE
 #if BURST_TEST_OVERRIDE_AVX2
     private static bool IsAvx2Supported => true;
-#endif
-#if BURST_TEST_OVERRIDE_SSE41
-    private static bool IsSse41Supported => true;
-#endif
-
-#if BURST_TEST_OVERRIDE_SSE2
-    private static bool IsSse2Supported => true;
-#endif
 #endif
 
     public static void SolveTableau(Collections.Matrix tableau, Collections.BitSet selected)
@@ -181,56 +172,6 @@ internal static partial class Simplex
             else
                 return (int)extract_epi64(hpivot, 1);
         }
-        else if (IsSse2Supported)
-        {
-            var vpivot = set1_epi64x(-1);
-            var vvalue = set1_pd(0.0);
-            var indices = set_epi64x(1, 0);
-
-            int i = 0;
-            for (; i + 2 <= length; i += 2)
-            {
-                var current = loadu_si128(&row[i]);
-                var mask = cmplt_pd(current, vvalue);
-
-                if (IsSse41Supported)
-                    vpivot = blendv_pd(vpivot, indices, mask);
-                else
-                    vpivot = or_pd(andnot_pd(mask, vpivot), and_pd(mask, indices));
-
-                vvalue = min_pd(current, vvalue);
-                indices = add_epi64(indices, set1_epi64x(2));
-            }
-
-            double lo = cvtsd_f64(vvalue);
-            double hi = cvtsd_f64(unpackhi_pd(vvalue, vvalue));
-
-            double value;
-            int pivot;
-
-            if (lo <= hi)
-            {
-                value = lo;
-                pivot = (int)extract_epi64(vpivot, 0);
-            }
-            else
-            {
-                value = hi;
-                pivot = (int)extract_epi64(vpivot, 1);
-            }
-
-            if (i < length)
-            {
-                double current = row[i];
-                if (current < value)
-                {
-                    value = current;
-                    pivot = i;
-                }
-            }
-
-            return pivot;
-        }
         else
         {
             int pivot = -1;
@@ -309,16 +250,6 @@ internal static partial class Simplex
             if (i < length)
                 row[i] /= scale;
         }
-        else if (IsSse2Supported)
-        {
-            var vscale = set1_pd(scale);
-
-            for (; i <= length - 2; i += 2)
-                storeu_si128(&row[i], div_pd(loadu_si128(&row[i]), vscale));
-
-            if (i < length)
-                row[i] /= scale;
-        }
         else
         {
             for (; i < length; ++i)
@@ -359,32 +290,6 @@ internal static partial class Simplex
 
                 mm256_storeu_pd(&dst[i], mm256_andnot_pd(mask, r));
             }
-
-            Hint.Assume(length - i < 4);
-        }
-
-        if (IsSse2Supported)
-        {
-            v128 vscale = set1_pd(scale);
-            v128 epsilon = set1_pd(Epsilon);
-
-            for (; i + 2 <= length; i += 2)
-            {
-                v128 d = loadu_si128(&dst[i]);
-                v128 s = mul_pd(loadu_si128(&src[i]), vscale);
-                v128 r = sub_pd(d, s);
-
-                v128 absr = abs_pd(r);
-                v128 rel = div_pd(absr, add_pd(abs_pd(d), abs_pd(s)));
-
-                v128 mask1 = cmpge_pd(absr, epsilon);
-                v128 mask2 = cmpge_pd(rel, epsilon);
-                v128 mask = or_pd(mask1, mask2);
-
-                storeu_si128(&dst[i], and_pd(r, mask));
-            }
-
-            Hint.Assume(length - i < 2);
         }
 
         for (; i < length; ++i)
@@ -404,19 +309,6 @@ internal static partial class Simplex
             // If d and s almost perfectly cancel out then just truncate to 0.
             if (Math.Abs(r) / (Math.Abs(d) + Math.Abs(s)) < Epsilon)
                 dst[i] = 0.0;
-        }
-    }
-
-    [IgnoreWarning(1370)]
-    private static v256 mm256_cmpge_epi64(v256 a, v256 b)
-    {
-        if (IsAvx2Supported)
-        {
-            return mm256_andnot_si256(mm256_cmpgt_epi64(a, b), mm256_set1_epi8(0xFF));
-        }
-        else
-        {
-            throw UnsupportedInstructionSet();
         }
     }
 
@@ -444,21 +336,6 @@ internal static partial class Simplex
         if (IsAvx2Supported)
         {
             return mm256_andnot_pd(mm256_set1_pd(-0.0), x);
-        }
-        else
-        {
-            throw UnsupportedInstructionSet();
-        }
-    }
-
-    [IgnoreWarning(1370)]
-    private static v256 mm256_select_si256(v256 mask, v256 a, v256 b)
-    {
-        if (IsAvx2Supported)
-        {
-            var sa = mm256_and_si256(mask, a);
-            var sb = mm256_andnot_si256(mask, b);
-            return mm256_or_si256(sa, sb);
         }
         else
         {
