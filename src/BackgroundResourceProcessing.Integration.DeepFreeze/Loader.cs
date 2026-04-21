3e6c9915683af765705a12fd5f3ea137d09a9e77
Phantomical <phantom@lynches.ca>
2026-04-20 22:08:55 -0700

    Make the status window use BRP's simulator

--- src/BackgroundResourceProcessing.Integration.DeepFreeze/Loader.cs
+++ src/BackgroundResourceProcessing.Integration.DeepFreeze/Loader.cs
@@ -6,9 +6,17 @@ namespace BackgroundResourceProcessing.Integration.DeepFreeze;
 [KSPAddon(KSPAddon.Startup.Instantly, true)]
 public class Loader : MonoBehaviour
 {
+    private static readonly Harmony harmony = new(
+        "BackgroundResourceProcessing.Integration.DeepFreeze"
+    );
+
     void Awake()
     {
-        var harmony = new Harmony("BackgroundResourceProcessing.Integration.DeepFreeze");
         harmony.PatchAll(typeof(Loader).Assembly);
+    }
+
+    static void OnHotUnload()
+    {
+        harmony.UnpatchAll(harmony.Id);
     }
 }
